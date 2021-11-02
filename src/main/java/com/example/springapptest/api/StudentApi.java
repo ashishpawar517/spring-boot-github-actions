6b2a68d21a7899c248848d4671de55bb6db3fc44
Ashish <pawarashish564@gmail.com>
2021-11-02 19:20:03 +0530

    fix: new changes

--- src/main/java/com/example/springapptest/api/StudentApi.java
+++ src/main/java/com/example/springapptest/api/StudentApi.java
@@ -2,6 +2,12 @@ package com.example.springapptest.api;
 
 import com.example.springapptest.model.Student;
 import com.example.springapptest.service.IStudentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,25 +27,40 @@ public class StudentApi {
     @Autowired
     IStudentService studentService;
 
+    @Operation(summary = "Find Count of Students", description = "Returns total number of Students", tags = {"Student"})
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(array = @ArraySchema(schema = @Schema(implementation = Student.class))))})
+    @GetMapping("/getStudent")
     @RequestMapping(value = "/getCount", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<?> getCount(){
+    public ResponseEntity<?> getCount() {
         return new ResponseEntity<>(studentService.getStudentCount(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Find Students by ID", description = "Name search by ID format", tags = {"Student"})
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(array = @ArraySchema(schema = @Schema(implementation = Student.class))))})
     @GetMapping("/getStudent")
-    public ResponseEntity<?> getStudent(@RequestParam("id") Long id){
-        return new ResponseEntity<>(studentService.getStudent(id),HttpStatus.OK);
+    public ResponseEntity<?> getStudent(@RequestParam("id") Long id) {
+        return new ResponseEntity<>(studentService.getStudent(id), HttpStatus.OK);
     }
 
+    @Operation(summary = "Dummy API", description = "return Hello World")
     @GetMapping("/greet")
-    public ResponseEntity<?> greet(){
-        return new ResponseEntity<>("hello world",HttpStatus.OK);
+    public ResponseEntity<?> greet() {
+        return new ResponseEntity<>("hello world x3", HttpStatus.OK);
     }
+
+    @Operation(summary = "Add a new Student", description = "", tags = { "Student" })
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Student created",
+            content = @Content(schema = @Schema(implementation = Student.class))),
+        @ApiResponse(responseCode = "400", description = "Invalid input"),
+        @ApiResponse(responseCode = "409", description = "Student already exists") })
     @PostMapping("/insertStudent")
-    public ResponseEntity<?> insertStudent(HttpServletRequest request,@RequestBody Student studentRequest,
-        BindingResult result){
+    public ResponseEntity<?> insertStudent(HttpServletRequest request, @RequestBody Student studentRequest,
+        BindingResult result) {
         studentService.insertStudent(studentRequest);
-        return new ResponseEntity<>("Student details added.",HttpStatus.OK);
+        return new ResponseEntity<>("Student details added.", HttpStatus.OK);
     }
 
 }
--- src/main/java/com/example/springapptest/model/Student.java
+++ src/main/java/com/example/springapptest/model/Student.java
@@ -1,5 +1,7 @@
 package com.example.springapptest.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,8 +17,14 @@ public class Student {
     public static final String COLUMN_NAME = "name";
     public static final String COLUMN_CGPA = "cgpa";
 
+    @Schema(description = "Unique identifier of the Student.", example = "1", required = true)
     private Long id;
+
+    @Schema(description = "Name  of the Student.", example = "ABC", required = true)
+    @Size(max = 100)
     private String name;
+
+    @Schema(description = "CGPA of the Student.", example = "9.2", required = true)
     private Float cgpa;
 
 
